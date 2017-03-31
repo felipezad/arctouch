3e3a5bdf63ad465b327be0b029a55d1883181ffa
Felipe Zadinello <felipezad@gmail.com>
2017-03-31 20:23:26 -0300

    MovieApi with all signatures needed

--- app/src/main/java/felipe/arctouch/tmdb/api/MovieAPI.java
+++ app/src/main/java/felipe/arctouch/tmdb/api/MovieAPI.java
@@ -1,8 +1,26 @@
 package felipe.arctouch.tmdb.api;
 
+import felipe.arctouch.tmdb.domain.Configuration;
+import felipe.arctouch.tmdb.domain.Images;
+import felipe.arctouch.tmdb.domain.Movie;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
 /**
  * Created by felipe on 30/03/17.
  */
 
-public class MovieAPI {
+public interface MovieAPI {
+
+    @GET("movie/upcoming")
+    Call<Movie> getUpcomingMovies(@Query("api_key") String apiKey, @Query("language") String language, @Query("page") Integer page);
+
+    @GET("configuration")
+    Call<Configuration> getImageConfiguration(@Query("api_key") String apiKey);
+
+    @GET("{baseUrl}/{size}/{filePath}")
+    Call<String> getUrlImage(@Path("baseUrl") String baseUrl, @Path("size") String size, @Path("filePath") String filePath);
+
 }
