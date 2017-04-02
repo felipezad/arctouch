25c0836085ea7bf1486828d6606cd587d4c2ebfa
Felipe Zadinello <felipezad@gmail.com>
2017-04-02 15:24:58 -0300

    Allowed to retrieve all kinds of genres

--- app/src/main/java/felipe/arctouch/tmdb/api/MovieAPI.java
+++ app/src/main/java/felipe/arctouch/tmdb/api/MovieAPI.java
@@ -1,6 +1,7 @@
 package felipe.arctouch.tmdb.api;
 
 import felipe.arctouch.tmdb.domain.Configuration;
+import felipe.arctouch.tmdb.domain.Genre;
 import felipe.arctouch.tmdb.domain.Movie;
 import retrofit.Call;
 import retrofit.http.GET;
@@ -16,6 +17,10 @@ public interface MovieAPI {
 
     @GET("movie/upcoming")
     Call<Movie> getUpcomingMovies(@Query("api_key") String apiKey, @Query("language") String language, @Query("page") Integer page);
+
+
+    @GET("genre/movie/list")
+    Call<Genre> getGenre(@Query("api_key") String apiKey, @Query("language") String language);
 
     @GET("configuration")
     Call<Configuration> getImageConfiguration(@Query("api_key") String apiKey);
